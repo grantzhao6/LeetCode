7594e14f939089dca83848299c570747c1eb9033
grantzhao6 <155490737+grantzhao6@users.noreply.github.com>
2024-12-12 02:15:38 -0500

    [20] [Time Beats: 100.00%] [Memory Beats: 12.25%] - LeetPush

---
+++ 20-Valid-Parentheses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for c in s:
+            if c == \]\:
+                if len(stack) == 0 or stack.pop() != \[\:
+                    return False
+            elif c == \)\:
+                if len(stack) == 0 or stack.pop() != \(\:
+                    return False
+            elif c == \}\:
+                if len(stack) == 0 or stack.pop() != \{\:
+                    return False
+            else:
+                stack.append(c)
+        
+        if len(stack) != 0:
+            return False
+
+        return True
+
+            
+        
